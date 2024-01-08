259cba42c46c897063986f769a854d6fdd17ade1
1985Valdemar <vteider@yahoo.com>
2024-01-08 13:34:06 -0300

    Inicio Python - DIO - 2024

---
+++ Inicio Python DIO/convertendo_tipos.py
@@ -0,0 +1,18 @@
+preco = 10.30
+print(preco,"= Aqui Float")
+
+# CONVERTENDO
+
+preco = int(preco)
+print(preco,"= Aqui Inteiro")
+
+
+preco = 10.50
+idade = 28
+
+print(str(preco))
+
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
---
+++ Inicio Python DIO/variaveis_constantes.py
@@ -0,0 +1,8 @@
+print(11 + 10 + 1000)
+print(True)
+print(False)
+print("Python")
+
+age = 38
+name = 'Valdemar'
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
